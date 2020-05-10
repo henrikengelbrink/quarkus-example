dd8e48a8762acb6a37ad215f721cefdfe3fa1bf2
henrikengelbrink <henrik@keeet.io>
2020-05-10 13:53:24 +0200

    fix: debug docker

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#mvn package -Pnative -Dquarkus.native.container-build=true
-#docker build -f src/main/docker/Dockerfile.native -t quarkus/app .
-docker run -i --rm -p 8080:8080 \
- -e POSTGRES_USER=postgres \
- -e POSTGRES_PASSWORD=postgres \
- -e POSTGRES_URL=jdbc:postgresql://docker.for.mac.host.internal:5432/postgres \
- quarkus/app
+mvn package -Pnative -Dquarkus.native.container-build=true
+docker build -f src/main/docker/Dockerfile.native -t quarkus/app .
+#docker run -i --rm -p 8080:8080 \
+# -e POSTGRES_USER=postgres \
+# -e POSTGRES_PASSWORD=postgres \
+# -e POSTGRES_URL=jdbc:postgresql://docker.for.mac.host.internal:5432/postgres \
+# quarkus/app
